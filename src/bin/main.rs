57a8cef931ecb3c49cef1f49df9a075cc1baf1a4
Xuanwo <github@xuanwo.io>
2022-02-02 18:55:33 +0800

    Add basic clap layout
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Init,
+    Status,
+    Commit,
+    Add { name: Option<String> },
+    Push,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { name } => println!("Hello, {}!", name.unwrap_or("World".to_string())),
+        _ => todo!()
+    }
 }
